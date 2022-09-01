450e8f5240303963d8bb0027610380d3986dcdb1
Kylian V <50665740+DevKyky@users.noreply.github.com>
2022-09-01 09:17:30 +0200

    Data

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Serie;
+use App\Repository\SerieRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,29 +15,68 @@ use Symfony\Component\Routing\Annotation\Route;
 class SerieController extends AbstractController
 {
     #[Route('', name: 'list')]
-    public function list(): Response
+    public function list(SerieRepository $serieRepository): Response
     {
-        // TODO renvoyer la liste des séries
-        return $this->render('serie/list.html.twig', [
+        // Récupère toutes les séries en BDD
+        // $series = $serieRepository->findAll();
+        //$series = $serieRepository->findBy([], ['popularity' => 'DESC'], 50);
+        $series = $serieRepository->findBestSeries(1);
 
+        return $this->render('serie/list.html.twig', [
+            "series" => $series
         ]);
     }
 
     #[Route('/{id}', name: 'show', requirements: ['id' => '\d+'])]
-    public function show(int $id): Response
+    #[ParamConverter('serie', class: 'App\Entity\Serie')]
+    public function show(SerieRepository $serieRepository, Serie $serie): Response
     {
-        dump($id);
+        // Sans ParamConverter
+/*        $serie = $serieRepository->find($id);
 
-        // TODO renvoyer détail d'une série
+        if(!$serie) {
+            throw $this->createNotFoundException("Ooops! Serie not found !");
+        }*/
+
         return $this->render('serie/show.html.twig', [
-            'noSerie' => $id
+            'serie' => $serie
         ]);
     }
 
     #[Route('/add', name: 'add')]
-    public function add(): Response
+    public function add(EntityManagerInterface $entityManager): Response
     {
         // TODO création d'une série
+
+        $serie = new Serie();
+        $serie->setBackdrop("azerty.png")
+            ->setDateCreated(new \DateTime())
+            ->setFirstAirDate(new \DateTime("-1 year"))
+            ->setGenres("SF")
+            ->setLastAirDate(new \DateTime())
+            ->setName("Stargate SG1")
+            ->setPopularity(9999)
+            ->setPoster("poster.png")
+            ->setStatus("Ended")
+            ->setTmdbId(1234)
+            ->setVote(10.0);
+
+        dump($serie);
+
+        $entityManager->persist($serie);
+        $entityManager->flush();
+
+        dump($serie);
+
+        $serie->setName("Stargate Atlantis");
+        $entityManager->persist($serie);
+        $entityManager->flush();
+
+        dump($serie);
+
+        $entityManager->remove($serie);
+        $entityManager->flush();
+
         return $this->render('serie/add.html.twig');
     }
 }
